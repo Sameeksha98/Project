ef5d161b868dc230a2c6631996d8f02393c2d34d
Sameeksha98 <51112223+Sameeksha98@users.noreply.github.com>
2021-04-08 17:57:52 +0530

    Add files via upload

---
+++ RestMockmvcTest/src/main/java/com/test/testcontrollerexample/HelloService.java
@@ -0,0 +1,12 @@
+package com.test.testcontrollerexample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+
+
+    public String hello() {
+        return "Hello World";
+    }
+}
